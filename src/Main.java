0fc8c700c36365c2cff7339b0de252be8b3caf9f
Саня <alecs.korotkow@mail.ru>
2023-09-05 22:21:45 +0300

    Test conflict 2

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         System.out.println(plus(f));
 
 
-        System.out.println("test");
+        System.out.println("test321");
         System.out.println("test");
     }
 }
